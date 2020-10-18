0988ebac2045cceab8cc92a3acd840266469aaf9
Dat <takexa123456@gmail.com>
2020-10-18 11:03:10 +0700

    init new react app

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Le Nguyen Tan Dat</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Le Nguyen Tan Dat
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
